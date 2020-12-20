a836b707c4c6499fb39e703caaecdbce4526f0cd
Alexandr Kobelchak <kobelchak@gmail.com>
2020-12-20 09:28:11 +0200

    start

---
+++ DemoASP.WebAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoASP.WebAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
